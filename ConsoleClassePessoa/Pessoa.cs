cd25b1af5caafc8d7eabfec99d1512946ec52775
vanessa.vml.vm@hotmail.com <vanessa.vml.vm@hotmail.com>
2021-10-15 16:16:36 -0300

    adding filles

---
+++ ConsoleClassePessoa/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClassePessoa
+{
+    class Pessoa
+    {
+        //atributos
+        public string nome;
+        public int idade;
+        //só será int quando precisar de calculos matemáticos
+        public string rg;
+        public string cpf;
+
+        //adicionando os gets e sets (encapsulamento)
+        //os gets e sets atruibuem movimento e comportamento para os atributos
+
+        //funçao dentro da classe
+        public void Acenar(Pessoa pessoa)
+        {
+            // o this pega o nome da pessoa que está acenando
+            //método
+            //pessoa=pessoaSilva, é uma cópia e o this é a pessoa a ser digitada
+            Console.WriteLine("{0} acenou para {1}", this.nome, pessoa.nome);
+        }
+
+        public void PerguntaIdade(Pessoa pessoa)
+        {
+            //pergunta para a pessoa silvia
+            Console.WriteLine("{0} quantos anos você tem?", pessoa.nome);
+            pessoa.RespostaIdade();
+        }
+
+        public void RespostaIdade()
+        {
+            Console.WriteLine("Eu tenho {0} anos", this.idade);
+        }
+    }
+}
---
+++ ConsoleClassePessoa/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleClassePessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //faz a referencia da classe Pessoa, pega todos os atributos da mesma
+            //Tipo da classe= Pessoa nome da variavel=pessoa novo objeto=new Pessoa();
+            //=new Pessoa-copia todos os atributos da classse Pessoa 
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Digite o nome");
+            //atribuindo valor aos atributos
+            pessoa.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a idade");
+            Console.Read();
+            pessoa.idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o rg");
+            Console.Read();
+
+            Console.WriteLine("Digite o cpf");
+            Console.Read();
+            pessoa.rg = Console.ReadLine();
+            pessoa.cpf = Console.ReadLine();
+
+            //acessando os atributos
+            Console.WriteLine("Nome: {0} ", pessoa.nome);
+            Console.WriteLine("Idade{0} ", pessoa.idade);
+            Console.WriteLine("RG: {0} ", pessoa.rg);
+            Console.WriteLine("CPF: {0} ", pessoa.cpf);
+
+            //armezena apenas somente uma pessoa
+            Pessoa pessoaSilva = new Pessoa();
+            //atribuindo valor aos atributos
+            pessoaSilva.nome = "Silvia Mendes";
+            pessoaSilva.idade = 52;
+            pessoaSilva.rg = "00.000.000-0";
+            pessoaSilva.cpf = "000.000.000-00";
+
+            //acessando os atributos
+            Console.WriteLine("Nome: {0} ", pessoaSilva.nome);
+            Console.WriteLine("Idade{0} ", pessoaSilva.idade);
+            Console.WriteLine("RG: {0} ", pessoaSilva.rg);
+            Console.WriteLine("CPF: {0} ", pessoaSilva.cpf);
+
+            //pessoa=pessoa que será digitada
+            pessoa.Acenar(pessoaSilva);
+            pessoa.PerguntaIdade(pessoaSilva);
+
+        }
+    }
+}
